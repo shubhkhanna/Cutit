177c3ea1d23f567fcf143469bb040332bd06704d
shubhkhanna <khannashubh2001@gmail.com>
2021-12-29 04:21:28 +0530

    feat: Add server.js

---
+++ server/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import connectDB from "./config/db.js";
+import urlRoutes from "./routes/urlRoutes.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", urlRoutes);
+
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running on PORT ${port}`);
+});
